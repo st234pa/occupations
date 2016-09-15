e24c7cf5e7413d5cdc0727c36eff086f7f819145
STmb2 <STmb2@STmb2s-MacBook-Air.local>
2016-09-15 15:04:36 -0400

    added select

--- occupations.py
+++ occupations.py
@@ -1,9 +1,17 @@
 import csv
+import random
 
 reader = csv.reader(open('occupations.csv', 'r'))
 d = {}
 for row in reader:
     k, v = row
     d[k] = v
-    print(row)
+  
+def select():
+    total = 0
+    for k in d:
+        total += d[k]
+        if random.random() * total < d[k]:
+            return d.keys(k)
 
+print(select())
